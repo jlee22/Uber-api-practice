22e86e3349092f9b78f5eb7e03bbf58b71229240
Jeffrey Lee <jfflee.22@gmail.com>
2017-02-26 22:41:47 -0800

    provide location and ajax function to return time estimates and geolocation

--- app/01-static-invitation/js/uber.js
+++ app/01-static-invitation/js/uber.js
@@ -3,10 +3,10 @@ var uberClientId = "ydW7vTGHvHjYhMI1X1KWj86oFqLIFZ71"
   , uberServerToken = "czYM-kgEUbw2AyaL2J9y-ftR4RkfU17tXidXNzUl";
 
 // create placeholder variables
-var userLatitude
-  , userLongitude;
-  , partyLatitude = 37.775214;
-  , partyLongitude = -122.417509;
+var userLatitude,
+    userLongitude,
+    partyLatitude = 37.775214,
+    partyLongitude = -122.417509;
 
 navigator.geolocation.watchPosition(function(position) {
     console.log(position);
@@ -15,3 +15,21 @@ navigator.geolocation.watchPosition(function(position) {
     userLatitude = position.coords.latitude;
     userLongitude = position.coords.longitude;
 });
+
+function getEstimatesForUserLocation(latitude,longitude) {
+  $.ajax({
+    url: "https://api.uber.com/v1/estimates/price",
+    headers: {
+        Authorization: "Token " + uberServerToken
+    },
+    data: {
+      start_latitude: latitude,
+      start_longitude: longitude,
+      end_latitude: partyLatitude,
+      end_longitude: partyLongitude
+    },
+    success: function(result) {
+      console.log(result);
+    }
+  });
+}
